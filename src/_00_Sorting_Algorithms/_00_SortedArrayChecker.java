af2119026dd2e6c27a49dba74f542fd07c42dba4
league <league@imac-14.attlocal.net>
2019-09-15 17:32:41 -0700

    sorted array checker ip

--- src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
+++ src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
@@ -1,18 +1,38 @@
 package _00_Sorting_Algorithms;
 
+import java.util.function.BooleanSupplier;
+
 public class _00_SortedArrayChecker {
 	//1. Write a static method called intArraySorted. 
 	//   This method takes in an array of integers
 	//   and it returns a boolean.
 	//   The method returns true if the integer
 	//   array is in ascending order and false otherwise
-
+	static boolean intArraySorted() {
+		int[] intArray = null;
+		for (int i = 0; i < intArray.length; i++) {
+			if(i > i-1) {									//not right
+				return true;
+			}
+		}
+		return false;
+		
+	}
 	
 	//2. Write a static method called doubleArraySorted. 
 	//   This method takes in an array of doubles
 	//   and it returns a boolean.
 	//   The method returns true if the double
 	//   array is in ascending order and false otherwise
+	static boolean doubleArraySorted(double[] doubleArray) {
+		for (int i = 0; i < doubleArray.length; i++) {
+			if(i >= doubleArray[i-1]) {
+				return true;
+			}
+		}
+		return false;
+		
+	}
 
 	
 	//3. Write a static method called charArraySorted. 
